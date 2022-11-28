b6297ea0cea4c0abf3bdad87851ec99558171fff
indrajau <119101363+indrajau@users.noreply.github.com>
2022-11-28 16:35:18 +0530

    task 5

--- fourthfile.js/demo4.js
+++ fourthfile.js/demo4.js
@@ -11,7 +11,6 @@ console.log(headerTitle.innerText);
 //console.log(headerTitle.innerHTML);
 headerTitle.innerHTML = '<h3>Hello</h3>';
 headerTitle.style.borderBottom = 'solid 3px #000';
-
 var addItems = document.getElementsByClassName('title')
 addItems[0].style.fontWeight = 'bold';
 addItems[0].style.color = 'green';
