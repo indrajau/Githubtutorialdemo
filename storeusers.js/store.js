ae3ba4cc49d6b5db86e305028ec9ed974200b7e0
indrajau <119101363+indrajau@users.noreply.github.com>
2022-12-04 13:13:09 +0530

    Update store.js

--- storeusers.js/store.js
+++ storeusers.js/store.js
@@ -1,28 +1,53 @@
-const myForm = document.querySelector("#my-form");
-const nameInput = document.querySelector("#name");
-const emailInput = document.querySelector("#email");
-const msg = document.querySelector(".msg");
-// Listen for form submit
-myForm.addEventListener("submit", onSubmit);
-function onSubmit(e) {
+
+var myForm = document.getElementById("my-form");
+var nameInput = document.querySelector("#name");
+var phoneInput = document.querySelector("#phone");
+var userList = document.getElementById("users");
+var msg = document.querySelector(".msg");
+
+myForm.addEventListener("submit", addItem);
+userList.addEventListener("click", removeItem);
+userList.addEventListener("click", editItem);
+function addItem(e) {
   e.preventDefault();
-  if (nameInput.value === "" || emailInput.value === "") {
-    // alert('Please enter all fields');
+  if (nameInput.value === "" || phoneInput.value === "") {
     msg.classList.add("error");
     msg.innerHTML = "Please enter all fields";
-    // Remove error after 3000 milliseconds
     setTimeout(() => msg.remove(), 3000);
   } else {
-    let object = {
+    const obj = {
       name: nameInput.value,
-      email: emailInput.value,
+      phone: phoneInput.value,
     };
-    let object_serialize = JSON.stringify(object);
-    localStorage.setItem("object", object_values);
-    let object_deserialize = JSON.parse(localStorage.getItem("object"));
-    console.log(localStorage);
-    // Clear fields
+    localStorage.setItem(obj.phone, JSON.stringify(obj));
+    var object = JSON.parse(localStorage.getItem(obj.phone));
+    var userDetails = `${object.name} : ${object.phone}`;
+    var li = document.createElement("li");
+    li.className = "list-group";
+    li.appendChild(document.createTextNode(userDetails));
+    var deleteBtn = document.createElement("button");
+    var editBtn = document.createElement("button");
+    editBtn.style.backgroundColor = "green";
+    deleteBtn.style.background = "red";
+    deleteBtn.className = "btn delete";
+    editBtn.className = "btn edit";
+    editBtn.appendChild(document.createTextNode("Edit"));
+    deleteBtn.appendChild(document.createTextNode("Delete"));
+    li.appendChild(editBtn);
+    li.appendChild(deleteBtn);
+    userList.appendChild(li);
     nameInput.value = "";
-    emailInput.value = "";
+    phoneInput.value = "";
+  }
+}
+function editItem(e) {
+  
+}
+function removeItem(e) {
+  if (e.target.classList.contains("delete")) {
+    if (confirm("Do you want to delete user from list")) {
+      var li = e.target.parentElement;
+      userList.removeChild(li);
+    }
   }
 }
